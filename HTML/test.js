daf8c6f8fd3f6d797fbb2a97cd2d924f49745333
ankvnbaz-design <ankvnbaz@gmail.com>
2026-09-22 10:33:29 +0700

    Add files via upload

---
+++ HTML/test.js
@@ -0,0 +1,8 @@
+let n = prompt ("Nhập số n : ");
+        let sum = 0;
+        for (let i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+                document.write (i + "<br>");
+            }
+        }
